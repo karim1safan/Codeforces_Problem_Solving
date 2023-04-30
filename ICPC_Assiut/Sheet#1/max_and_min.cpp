1945f603be4f5d02fc94e82a8a101a0b309ba7c6
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-04-30 21:22:01 +0200

    Create max_and_min.cpp

---
+++ ICPC_Assiut/Sheet#1/max_and_min.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+
+    int a,b,c;
+
+    cin >>a>>b>>c;
+
+    cout << min(a,min(b,c));
+
+    cout << " ";
+
+    cout << max(a,max(b,c));
+
+    return 0;
+}
