864b1a1201113365df0c52c3e73a4264a4194e6a
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-04-30 23:30:45 +0200

    Update winter_sale.cpp

--- ICPC_Assiut/Contest#1/winter_sale.cpp
+++ ICPC_Assiut/Contest#1/winter_sale.cpp
@@ -1,1 +1,18 @@
 
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    double x, p; // 20 80
+
+    cin >> x >> p;
+
+    double results = p/(1-(x/100));
+
+    cout << round(results * 100.0) / 100.0 << endl; 
+
+    return 0;
+}
