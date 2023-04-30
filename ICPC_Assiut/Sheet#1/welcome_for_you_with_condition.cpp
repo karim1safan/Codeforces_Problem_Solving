c65a9f72504b47f34e15cc9ba0ed067115620f96
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-04-30 21:20:07 +0200

    Create welcome_for_you_with_condition.cpp

---
+++ ICPC_Assiut/Sheet#1/welcome_for_you_with_condition.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+
+    if (a >= b)
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+
+    return 0;
+}
