8f6f2f49b3a30c02f24169bc7cc60a07e518374a
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-05-03 01:40:58 +0300

    Create give_me_cents.cpp

---
+++ ICPC_ZAGAZIG/Sheet_0(Introduction)/give_me_cents.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // 1 dollar => 100 cent 
+    // 120%100 => 20 the last of 2 digits
+    int n;
+    cin>>n;
+    cout<<n%100<<endl;
+    return 0;
+}
