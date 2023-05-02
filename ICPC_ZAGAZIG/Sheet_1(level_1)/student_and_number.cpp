06a830458bdd6bae92043adc518b3fadf028b661
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-05-03 01:49:48 +0300

    Create student_and_number.cpp

---
+++ ICPC_ZAGAZIG/Sheet_1(level_1)/student_and_number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    if(x==y)
+        cout << "=\n";
+    else if(x>y)
+        cout << ">\n";
+    else
+        cout << "<\n";
+    return 0;
+}
