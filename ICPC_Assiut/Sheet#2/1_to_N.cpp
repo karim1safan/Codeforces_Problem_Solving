4db546755f2d81bcb3324fbf2cbde9f1dd2bc49a
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-05-02 13:55:26 +0300

    Create 1_to_N.cpp

---
+++ ICPC_Assiut/Sheet#2/1_to_N.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; // 5
+
+    cin >> n;
+
+    int s = 1;
+    while(s <= n)
+    {
+        cout << s << endl;
+        s++;
+    }
+
+    return 0;
+}
