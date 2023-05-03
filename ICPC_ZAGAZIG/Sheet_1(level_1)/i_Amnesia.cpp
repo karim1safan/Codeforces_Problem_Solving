48eee1d0b94eda381e1b50c6587e72ef1446bfa3
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-05-03 04:35:31 +0300

    Create i_Amnesia.cpp

---
+++ ICPC_ZAGAZIG/Sheet_1(level_1)/i_Amnesia.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char c;
+    cin>>c;
+    if((c >= 'a' && c <= 'z') || c >= 'A' && c <= 'Z')
+        cout << "Alphabet\n";
+    else
+        cout << "Not alphabet\n";
+
+    return 0;
+}
