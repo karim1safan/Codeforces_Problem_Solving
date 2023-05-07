d33b61f6e64ddba3f32520b9477fbf860e67d56f
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-05-07 17:51:23 +0300

    Create Numbers_Histogram.cpp

---
+++ ICPC_Assiut/Sheet#2/Numbers_Histogram.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char s;
+    int t,n;
+    cin>>s;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        for(int i=1;i<=n;i++)
+        {
+            cout<<s;
+        }
+        cout<<endl;
+    }
+}
