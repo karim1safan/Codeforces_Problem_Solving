99467a0c7e483bf961c06e238fda0d6cc45e5219
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2023-05-11 22:44:47 +0300

    Create A. Timon and Pumbaa.cpp

---
+++ ICPC_Assiut/Contest#2/A. Timon and Pumbaa.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+    ll a,b;
+    cin >> a >>b;
+    if(a-b >= 0)
+    {
+        cout << a-b << endl;
+    }
+    else
+        cout << 0 << endl;
+}
